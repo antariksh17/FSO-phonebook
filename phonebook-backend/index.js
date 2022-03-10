24f42fe18d62d64cfcb4f4d772426c5f9b607763
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-10 20:58:43 +0530

    edits

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,6 +4,10 @@
 
     const app= express()
 
+    const cors = require('cors')
+    app.use(cors())
+
+
     
     app.use(express.json())
 
@@ -110,7 +114,7 @@
 
 
 
-    const PORT = 3001
+    const PORT = process.env.PORT || 3001
 
     app.listen(PORT, () => {
         console.log(`server running on port ${PORT}`)
