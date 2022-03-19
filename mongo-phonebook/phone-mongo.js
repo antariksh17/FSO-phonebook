d62c7ef8c6d38525dac0faca3202712c532b93c5
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-19 17:54:28 +0530

    completed 3.12

---
+++ mongo-phonebook/phone-mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node phone-mongo.js <password>')
+  process.exit(1)
+} else if(process.argv.length<4){
+    console.log('Please provide the data to be added as an argument:  node phone-mongo.js <password> <"firstname lastName"> <number> ')
+    process.exit(1)
+} else if(process.argv.length<5){
+    console.log('Please provide the number also to be added as an argument:  node phone-mongo.js <password> <"firstname lastName"> <number> ')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://antariksh:${password}@cluster0.ouytb.mongodb.net/phonebook?retryWrites=true&w=majority`
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+contact.save().then(result => {
+  console.log(`Added ${contact.name} number ${contact.number} to phonebook`)
+  mongoose.connection.close()
+})
+
+
+// Contact.find({}).then(result => {
+//     result.forEach(contact => {
+//       console.log(contact)
+//     })
+//     mongoose.connection.close()
+//   })
+
+
+
+
+
+
+
+//pass: jack$1 -> jack%241
