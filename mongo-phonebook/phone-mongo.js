2049eb3f0a5bd983146eb9d216e00ec2bdb940f4
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-19 18:06:27 +0530

    edited 3.12 fix

--- mongo-phonebook/phone-mongo.js
+++ mongo-phonebook/phone-mongo.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose')
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node phone-mongo.js <password>')
   process.exit(1)
-} else if(process.argv.length<4){
-    console.log('Please provide the data to be added as an argument:  node phone-mongo.js <password> <"firstname lastName"> <number> ')
-    process.exit(1)
-} else if(process.argv.length<5){
-    console.log('Please provide the number also to be added as an argument:  node phone-mongo.js <password> <"firstname lastName"> <number> ')
-    process.exit(1)
-}
+} 
+
 
 const password = process.argv[2]
 
@@ -29,19 +24,31 @@ const contact = new Contact({
     number: process.argv[4],
 })
 
-contact.save().then(result => {
-  console.log(`Added ${contact.name} number ${contact.number} to phonebook`)
-  mongoose.connection.close()
-})
 
 
-// Contact.find({}).then(result => {
-//     result.forEach(contact => {
-//       console.log(contact)
-//     })
-//     mongoose.connection.close()
-//   })
+if(process.argv.length==3){
 
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+        console.log(contact)
+        })
+        mongoose.connection.close()
+    })
+
+
+} else if(process.argv.length==4){
+
+    console.log('Please provide the number also to be added as an argument:  node phone-mongo.js <password> <"firstname lastName"> <number> ')
+    process.exit(1)
+
+} else if(rocess.argv.length==5){
+    
+    contact.save().then(result => {
+        console.log(`Added ${contact.name} number ${contact.number} to phonebook`)
+        mongoose.connection.close()
+      })
+      
+}
 
 
 
