c03e15def76c9f814febc52f98a4f3137435118d
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-21 20:32:56 +0530

    lint edits

--- index.js
+++ index.js
@@ -1,204 +1,208 @@
-    require('dotenv').config()
-    
-    const express = require('express')
+require('dotenv').config()
 
-    const app= express()
+const express = require('express')
 
-    const morgan = require('morgan')
+const app= express()
 
-    const cors = require('cors')
+const morgan = require('morgan')
 
-    const Contact = require('./models/contact')
-const contact = require('./models/contact')
+const cors = require('cors')
 
-    app.use(express.json())
-
-    app.use(cors())
+const Contact = require('./models/contact')
 
 
-    app.use(express.static('build'))
-    
+app.use(express.json())
 
-    morgan.token('data', function(req, res) {
-
-        
-        return JSON.stringify(req.body);
-    })
-
-    app.use(
-        morgan(":method :url :status :res[content-length] - :response-time ms :data")
-    )
-
-    
-    
+app.use(cors())
 
 
-    app.get('/api/persons', (request, response) => {
-        
-        Contact.find({}).then(contacts => {
+app.use(express.static('build'))
 
-            response.json(contacts)
-      
-        })
-        
-        .catch(error => next(error))
-    })
 
-    app.get('/info', (request, response) => {
-        
-        Contact.find({}).then(contacts => {
-            
-            response.json(`Phonebook has info for ${persons.length} people ${new Date()}`)
+// eslint-disable-next-line no-unused-vars
+morgan.token('data', function(req, res) {
+
+
+  return JSON.stringify(req.body)
+})
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
 
 
-        })
-        
-        
-       
-    })
-
-    app.get('/api/persons/:id', (request, response,next) => {
-        
-        Contact.findById(request.params.id).then(contact => {
-    
-            if(contact) {
-              response.json(contact)
-            } else {
-              response.status(404).end()
-            }
-        })
-        
-        .catch(error => next(error))
-        
 
 
-    })
-   
+app.get('/api/persons', (request, response) => {
 
-    
+  Contact.find({}).then(contacts => {
 
-        
-    
-            
-              
-    app.put(`/api/persons/:id`, (request, response, next) => {
-        const body = request.body
+    response.json(contacts)
 
-        const contact = ({
-          
-            name: body.name,
-            number: body.number
-              
-        })
+  })
 
-        Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
-                
-        .then(updatedContact => {
-            response.json(updatedContact)
-        })
-            
-        .catch(error => next(error))
-    })
+    // eslint-disable-next-line no-undef
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response) => {
+
+  Contact.find({}).then(contacts => {
+
+    response.json(`Phonebook has info for ${contacts.length} people ${new Date()}`)
 
 
-            
-        
-    
 
-    app.post('/api/persons', (request,response, next)=> {
-        const body = request.body
-
-        if (body.name === undefined) {
-          return response.status(400).json({ error: 'content missing' })
-        }
-      
-        const contact = new Contact({
-          
-            name: body.name,
-            number: body.number
-          
-        })
+  })
 
 
-        Contact.findOne({name:body.name}).then(contacts => {
-    
-            if(contacts) {
-              
-                return response.status(400).json({ error: 'Contact already exists in DB' })
 
-            } else {
-                
-              
-                contact.save().then(savedContact => {
-                  response.json(savedContact)
-                })
+})
 
-                .catch((error) => next(error))
-            }
-        })
-        
-    })
+app.get('/api/persons/:id', (request, response,next) => {
 
-    app.delete('/api/persons/:id', (request,response, next) => {
+  Contact.findById(request.params.id).then(contact => {
 
-        Contact.findByIdAndRemove(request.params.id)
-            .then(result => {
-            response.status(204).end()
-        })
-        
-        .catch(error => next(error))
-
-    })
-
-    const unknownEndpoint = (request, response) => {
-        response.status(404).send({ error: 'unknown endpoint' })
+    if(contact) {
+      response.json(contact)
+    } else {
+      response.status(404).end()
     }
-      
-    app.use(unknownEndpoint)
-      
-    const errorHandler = (error, request, response, next) => {
-        console.error(error.message)
-      
-        if (error.name === 'CastError') {
-            return response.status(400).send({ error: 'malformatted id' })
-          } else if (error.name === 'ValidationError') {
-            return response.status(400).json({ error: error.message })
-        }
-      
-        next(error)
-    }
-    
-    app.use(errorHandler)
+  })
 
-    const PORT = process.env.PORT
+    .catch(error => next(error))
 
-    app.listen(PORT, () => {
-        console.log(`server running on port ${PORT}`)
+
+
+})
+
+
+
+
+
+
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const contact = ({
+
+    name: body.name,
+    number: body.number
+
+  })
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+
+    .then(updatedContact => {
+      response.json(updatedContact)
     })
+
+    .catch(error => next(error))
+})
+
+
+
+
+
+
+app.post('/api/persons', (request,response, next) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const contact = new Contact({
+
+    name: body.name,
+    number: body.number
+
+  })
+
+
+  Contact.findOne({ name:body.name }).then(contacts => {
+
+    if(contacts) {
+
+      return response.status(400).json({ error: 'Contact already exists in DB' })
+
+    } else {
+
+
+      contact.save().then(savedContact => {
+        response.json(savedContact)
+      })
+
+        .catch((error) => next(error))
+    }
+  })
+
+})
+
+app.delete('/api/persons/:id', (request,response, next) => {
+
+  Contact.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      response.status(204).end()
+    })
+
+    .catch(error => next(error))
+
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
 
 
 //-------------------------------------------
 
 // let persons = [
-//     { 
+//     {
 //     "id": 1,
-//     "name": "Arto Hellas", 
+//     "name": "Arto Hellas",
 //     "number": "040-123456"
 //     },
-//     { 
+//     {
 //     "id": 2,
-//     "name": "Ada Lovelace", 
+//     "name": "Ada Lovelace",
 //     "number": "39-44-5323523"
 //     },
-//     { 
+//     {
 //     "id": 3,
-//     "name": "Dan Abramov", 
+//     "name": "Dan Abramov",
 //     "number": "12-43-234345"
 //     },
-//     { 
+//     {
 //     "id": 4,
-//     "name": "Mary Poppendieck", 
+//     "name": "Mary Poppendieck",
 //     "number": "39-23-6423122"
 //     }
 // ]
